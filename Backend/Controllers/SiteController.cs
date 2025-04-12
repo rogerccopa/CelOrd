5fbcc6d39d455687db5cdd1fa6c916050a799473
Roger Ccopa <rogerccopa@delllaptop.com>
2025-04-11 22:13:55 -0500

    changes done on DELL

--- Backend/Controllers/SiteController.cs
+++ Backend/Controllers/SiteController.cs
@@ -4,23 +4,22 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Backend.Controllers
 {
-	[ApiController]
-	[Route("api/{controller}")]
-	public class SiteController(
-		ILogger<AuthController> logger,
-		IRepository repository,
-		IDataProtectionProvider dpProvider) : ControllerBase
-	{
-		private readonly ILogger<AuthController> _logger = logger;
-		private readonly IRepository _repo = repository;
-		private readonly IDataProtectionProvider _dpProvider = dpProvider;
+    [ApiController]
+    [Route("api/{controller}")]
+    public class SiteController(
+        ILogger<AuthController> logger,
+        IRepository repository,
+        IDataProtectionProvider dpProvider) : ControllerBase
+    {
+        private readonly ILogger<AuthController> _logger = logger;
+        private readonly IRepository _repo = repository;
+        private readonly IDataProtectionProvider _dpProvider = dpProvider;
 
-		[HttpPost("contactus")]
-		public IActionResult ContactUs()
-		{
-			// CONTINUE HERE...
-
-			return View();
-		}
-	}
+        [HttpPost("contactus")]
+        public IActionResult ContactUs()
+        {
+            var result = Models.Result<string>.Success("We received your message");
+            return Ok(result);
+        }
+    }
 }
--- Backend/Models/Result.cs
+++ Backend/Models/Result.cs
@@ -2,31 +2,31 @@
 
 public class Result<TValue> where TValue : class, new()
 {
-	public bool IsSuccess { get { return _isSuccess; } }
-	public bool IsFailure => !IsSuccess;
-	public string Error { get { return _error; } }
-	public TValue Value { get { return _value; } }
+    public bool IsSuccess { get { return _isSuccess; } }
+    public bool IsFailure => !IsSuccess;
+    public string Error { get { return _error; } }
+    public TValue Value { get { return _value; } }
 
-	private readonly bool _isSuccess;
+    private readonly bool _isSuccess;
 
-	private readonly TValue _value;
-	private readonly string _error;
+    private readonly TValue _value;
+    private readonly string _error;
 
-	private Result(TValue value)
-	{
-		_isSuccess = true;
-		_value = value;
-		_error = string.Empty;
-	}
+    private Result(TValue value)
+    {
+        _isSuccess = true;
+        _value = value;
+        _error = string.Empty;
+    }
 
-	private Result(string error)
-	{
-		_isSuccess = false;
-		_value = new TValue();
-		_error = error;
-	}
+    private Result(string error)
+    {
+        _isSuccess = false;
+        _value = new TValue();
+        _error = error;
+    }
 
-	public static Result<TValue> Success(TValue value) => new Result<TValue>(value);
+    public static Result<TValue> Success(TValue value) => new Result<TValue>(value);
 
-	public static Result<TValue> Failure(string error) => new Result<TValue>(error);
+    public static Result<TValue> Failure(string error) => new Result<TValue>(error);
 }
